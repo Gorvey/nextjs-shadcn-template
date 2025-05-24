417f8d42321ba62299d18992e12c874c35b81423
Gorvey <gorvey76@outlook.com>
2025-05-24 22:05:42 +0800

    add shadcn husky

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -77,6 +77,7 @@ export default function Home() {
           <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
           Go to nextjs.org →
         </a>
+        <Button>Click me</Button>
       </footer>
     </div>
   )
